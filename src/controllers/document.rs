53fcb4826cdf2af7aebc82bfc2e7e17b366f5b66
Hanz <haniel56@zoho.eu>
2025-01-19 16:11:04 +0530

    chore: update

--- src/controllers/document.rs
+++ src/controllers/document.rs
@@ -2,8 +2,48 @@
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
 use loco_rs::prelude::*;
+
 // use axum::debug_handler;
+use std::collections::HashMap;
+use std::sync::OnceLock;
+use tracing::error;
+
+
+// !### ### route configuration ### ### //
+const PREFIX: &str = "documents";
+
+fn get_routes() -> &'static HashMap<&'static str, &'static str> {
+    static ROUTES: OnceLock<HashMap<&'static str, &'static str>> = OnceLock::new();
+
+    ROUTES.get_or_init(|| {
+        // define routes here, and reference everywhere you need it.
+        HashMap::from([
+            ("index", "/"),
+            ("upload", "/upload"),
+            ("upload_status", "/upload/status"),
+            ("get_all", "/get/all"),
+            ("get", "/get"),
+            ("delete", "/delete"),
+        ])
+    })
+}
+
+fn get_routes_with_prefix(key: &str) -> String {
+    let route = match get_routes().get(key) {
+        Some(route) => route,
+        None => {
+            error!("get_routes({}) not found", key);
+            return "".to_string();
+        }
+    };
+    format!("/{}{}", &PREFIX, route)
+}
+
+// !### ### end route configuration ### ### //
+
 
 pub fn routes() -> Routes {
-    Routes::new().prefix("documents")
+    let r = get_routes();
+
+    Routes::new().prefix(PREFIX)
 }
